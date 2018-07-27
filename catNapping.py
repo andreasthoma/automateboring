fffe669c0005375c31d77ed69a915bbaca53d290
Andreas <athoma1@mac.com>
2018-07-27 15:15:46 -0700

    Initial commit

---
+++ catNapping.py
@@ -0,0 +1,9 @@
+print('''Dear Alice,
+
+This cat has been caught napping and it's complete bullshit.  He shouldn't be napping at work.
+
+I do find solace in that he is a cat and would be worse if he were human.
+
+Cheers,
+Andreas
+''')
