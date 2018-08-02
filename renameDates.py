c0649f5ae344666c437a52ca6a38ef8d69fbc3d6
Andreas <athoma1@mac.com>
2018-08-01 21:49:32 -0700

    change dates on all files
    Shell Utilities

---
+++ renameDates.py
@@ -0,0 +1,13 @@
+#! python3
+# renameDates.py - Renames filenames with American MM-DD-YYYY date format to European DD-MM-YYYY.
+
+import shutil, os, re
+
+# Create a regex that matches files with the American date format.
+
+datePattern = re.compile(r"""^(.*?)
+    ((0|1?\d)-
+    ((0|1|2|3)?\d)-
+    ((19|20)\d\d)
+    (.*?)$
+    """, re.VERBOSE)
