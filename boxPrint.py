0e30e29b703f2a6f292323506dd425d8ac83accd
Andreas <athoma1@mac.com>
2018-08-05 12:26:22 -0700

    Logging Errors
    Logging errors

--- boxPrint.py
+++ boxPrint.py
@@ -1,4 +1,4 @@
-def boxPrint(symbol, wiedth, height):
+def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception('Symbol must be a single chracter string.')
     if width <= 2:
@@ -7,5 +7,11 @@ def boxPrint(symbol, wiedth, height):
         raise Exception('Height must be greater than 2.')
     print(symbol * width)
     for i in range(height - 2):
-        print(symbol _ (' ' * (width - 2)) + symbol)
+        print(symbol + (' ' * (width - 2)) + symbol)
     print(symbol * width)
+
+for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+    try:
+        boxPrint(sym, w, h)
+    except Exception as err:
+        print('An exception happened: ' + str(err))
