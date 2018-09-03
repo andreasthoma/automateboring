9a04a0843b02b59b25ffe57d1d15c254d996e613
Andreas <athoma1@mac.com>
2018-09-03 12:46:04 -0700

    quick weather
    weather api

---
+++ quickWeather.py
@@ -0,0 +1,16 @@
+#! python3
+# quickWeather.py - Prints the weather for a location from the command line.
+
+import json, requests, sys
+
+# compute location from command line arguments.
+if len(sys.argv) < 2:
+    print('Usage: quickWeather.py location')
+    sys.exit()
+location = ' '.join(sys.argv[1:])
+
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+response = requests.get(url)
+response.raise_for_status()
+
+weatherData = json.loads(response.text)
